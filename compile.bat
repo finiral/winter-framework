7f1e31635b6e909e3a91a46c761d9b61d54da97c
finiral <finiral@yahoo.fr>
2024-05-07 12:03:13 +0300

    front controller

---
+++ compile.bat
@@ -0,0 +1,15 @@
+set destination=C:\Users\USER\College\S4\Web_Dynamique\Frameworks\winter\Fw\compiled
+set nomJar=winter-1
+set nomSrcTxt=C:\Users\USER\College\S4\Web_Dynamique\Frameworks\winter\Fw\winter-framework\java_files_list.txt
+set lib=C:\apache-tomcat-10.1.23\apache-tomcat-10.1.23\lib\servlet-api.jar
+call .\findJavaSrc
+set src=
+for /f "delims=" %%i in (%nomSrcTxt%) do set src=%src% %%i
+
+javac -cp "%lib%" -d %destination% %src%
+
+cd /d %destination%  && jar -cvf %nomJar%.jar *
+if exist %nomSrcTxt% (
+    del %nomSrcTxt%
+)
+cmd /k
---
+++ src/mg/itu/prom16/controller/FrontController.java
@@ -0,0 +1,30 @@
+package mg.itu.prom16.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+public class FrontController extends HttpServlet {
+    
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        processRequest(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        processRequest(req, resp);
+    }
+
+    protected void processRequest(HttpServletRequest request , HttpServletResponse response) throws ServletException,IOException{
+        PrintWriter out=response.getWriter();
+        StringBuffer url=request.getRequestURL();
+        out.println("L'URL EST :"+url);
+    }
+}
