beba97d96ae040f4661b77745dfbb3bbbcdbc3a4
finiral <finiral@yahoo.fr>
2024-09-27 09:00:59 +0300

    annotation sur la classe

--- src/mg/itu/prom16/annotations/RestAPI.java
+++ src/mg/itu/prom16/annotations/RestAPI.java
@@ -3,7 +3,7 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 import java.lang.annotation.RetentionPolicy;
-@Target(ElementType.METHOD)
+@Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface RestAPI {
 }
